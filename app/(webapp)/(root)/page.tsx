d9689809d12e65271e2989c95bf953a5be614510
Chuong Ngo <cngo.github@gmail.com>
2025-07-01 20:55:20 -0400

    Added header.

--- app/(webapp)/(root)/page.tsx
+++ app/(webapp)/(root)/page.tsx
@@ -1,5 +1,17 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1 className="heading">Home</h1>;
+  return (
+    <section className="pink_container pattern">
+      <h1 className="heading">
+        Pitch Your Startup
+        <br />
+        Connect With Entrepenuers
+      </h1>
+
+      <p className="sub-heading !max-w-3xl">
+        Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+      </p>
+    </section>
+  );
 }
